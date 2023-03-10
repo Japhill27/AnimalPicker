6f376861554dec7a1013a58c9a184de39959a90f
James <jrphillips@quinnipiac.edu>
2023-03-10 13:08:15 -0500

    ascii animal options

--- animal/AnimalPicker.java
+++ animal/AnimalPicker.java
@@ -1,5 +1,28 @@
 package animal;
 
-public class AnimalPicker {
+import java.util.Scanner;
 
+public class AnimalPicker {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		String animal;
+		System.out.println("What animal would you like to see: dog, cat, or fish?");
+		animal = input.nextLine();
+		while (!animal.equals("dog") || !animal.equals("cat") || !animal.equals("fish")) {
+			if (animal.equals("dog")) {
+				System.out.println(" __ _\no\'\')}____//\n `_/)\n (_(_/-(_/");
+				break;
+			} else if (animal.equals("cat")) {
+				System.out.println(" ^~^ ,\n (\'Y\') )\n / \\/\n(\\|||/)");
+				break;
+			} else if (animal.equals("fish")) {
+				System.out.println(" O o\n _\\_ o\n>(\'>\\\\/ o\\ .\n //\\___=\n \'\'");
+				break;
+			} else {
+				System.out.println("Please enter 'dog', 'cat', 'fish', or 'bear'");
+				animal = input.nextLine();
+			}
+		}
+
+	}
 }
